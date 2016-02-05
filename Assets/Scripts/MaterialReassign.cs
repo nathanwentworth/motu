095ff6192cac7f852218d255c8b824ca25cea770
Connor Botts <connor.botts@gmail.com>
2016-02-04 21:21:28 -0500

    CameraWorksInEditor

---
+++ Assets/Scripts/MaterialReassign.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MaterialReassign : MonoBehaviour {
+
+	void Update(){
+		AssetDatabase.Refresh ();
+		Texture newpic = Resources.Load("Screenshot", typeof(Texture)) as Texture; 
+		gameObject.GetComponent<Renderer> ().material.SetTexture ("_MainText", newpic); 
+	}
+}
---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ScreenShot : MonoBehaviour {
+
+	void LateUpdate() {
+		if(Input.GetButton("Fire1")){
+			Application.CaptureScreenshot("Assets\\Resources\\Screenshot.png");
+			Debug.Log ("*Camera Sound*");
+		}
+	}
+}
