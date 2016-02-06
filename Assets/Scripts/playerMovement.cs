d642272b40766cd3fe047d14af27a93c42532849
Nathan Wentworth <nwentworth95@gmail.com>
2016-02-06 09:06:00 -0500

    changed first person controller
    replaced the unity standard asset with first person drifter

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	private float runSpeed;
+	private float walkSpeed;
+	private float jumpForce;
+
+	private bool canRecieveInput;
+	private bool canJump;
+
+	private bool isMoving;
+
+	void Start () {
+		canRecieveInput = true;
+		runSpeed = 600;
+		walkSpeed = 3;
+		jumpForce = 10;	
+	}
+	
+	void FixedUpdate () {
+
+
+
+		if (canRecieveInput) {
+			float mHorizontal = Input.GetAxis("Horizontal");
+			float mVertical = Input.GetAxis("Vertical");
+
+			if (!Input.GetButton("Crouch")) {
+				GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * mVertical * runSpeed * Time.deltaTime);
+			}
+			else {
+				GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * mHorizontal * walkSpeed * Time.deltaTime);
+			}
+
+
+
+		}
+	} // end FixedUpdate
+	
+}
