10b5153fe6837b6b07bea40818f784bf2b7559e7
Nathan Wentworth <nwentworth95@gmail.com>
2016-02-06 14:25:14 -0500

    deleted stupid stuff

---
+++ Assets/Scripts/player/LockMouse.cs
@@ -0,0 +1,29 @@
+﻿// by @torahhorse
+
+using UnityEngine;
+using System.Collections;
+
+public class LockMouse : MonoBehaviour
+{	
+	void Start()
+	{
+  	Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+    void Update()
+    {
+    	// lock when mouse is clicked
+    	if( Input.GetMouseButtonDown(0) && Time.timeScale > 0.0f ) {
+	    	Cursor.visible = false;
+	 			Cursor.lockState = CursorLockMode.Locked;
+    	}
+    
+    	// unlock when escape is hit
+        if  (Input.GetKeyDown(KeyCode.Escape) )
+        {
+		    	Cursor.visible = true;
+	    		Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
