042c7055c1e1891810ab93bf90ea0425f5cd34d3
Connor Botts <connor.botts@gmail.com>
2016-02-08 14:53:22 -0500

    I did it dad

--- Assets/Scripts/MaterialReassign.cs
+++ Assets/Scripts/MaterialReassign.cs
@@ -4,22 +4,45 @@ using System.IO;
 
 public class MaterialReassign : MonoBehaviour {
 
-	private string url;
+	private string SDPath;
+	private string other;
+	public ScreenShot screenshot;
 
 	void Start(){
-		url = "file://" + Application.dataPath +"/Resources/Screenshot.png";
-		print (url);
+		SDPath = "file://" + Application.dataPath +"/Resources/StretchDog.png";
+		other = "file://" + Application.dataPath +"/Resources/Other.png";
 	}
 
 	void Update(){
 		if (Input.GetButton ("Fire1")) {
-			StartCoroutine ("LoadImage");
+			switch (screenshot.whatAnimal) {
+			case "stretchdog":
+				if (this.gameObject.tag == "StretchDogScreen") {
+					StartCoroutine ("LoadImageStretchDog");
+				}
+				break;
+
+			case "other":
+				if (this.gameObject.tag == "OtherScreen") {
+					StartCoroutine ("LoadImageOther");
+				}
+				break;
+			}
 		}
 	}
 
-	IEnumerator LoadImage(){
+	IEnumerator LoadImageStretchDog(){
 		Texture2D image = new Texture2D (2, 2);
-		WWW www = new WWW(url);
+		WWW www = new WWW(SDPath);
+		yield return www;
+		www.LoadImageIntoTexture (image);
+		gameObject.GetComponent<Renderer> ().material.mainTexture = image;
+		yield return null;
+	}
+
+	IEnumerator LoadImageOther(){
+		Texture2D image = new Texture2D (2, 2);
+		WWW www = new WWW(other);
 		yield return www;
 		www.LoadImageIntoTexture (image);
 		gameObject.GetComponent<Renderer> ().material.mainTexture = image;
