becf1a1a15181a55e7ead5d3b58aa7d9977547e9
Nathan Wentworth <nwentworth95@gmail.com>
2016-04-07 19:56:17 -0400

    0.8
    fixed anchors, font sizes, included scav hunt

---
+++ Assets/Scripts/scavItems.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class scavItems : IComparable<scavItems> {
+
+	public string nameItem;
+	public string nameReadable;
+
+	public scavItems(string newNameItem, string newNameReadable) {
+		nameItem = newNameItem;
+		nameReadable = newNameReadable;
+	}
+
+	public int CompareTo(scavItems other) {
+		if (other == null) {
+			return 1;
+		}
+		return 0;
+	}
+}
