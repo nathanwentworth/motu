cb02a06d3019141cc1760cbc0a4b3356e32a96b9
Connor Botts <connor.botts@gmail.com>
2016-04-11 13:40:23 -0400

    Options 1.0.1
    All options save to player prefs, and the fullscreen drop down actually
    works 8)

--- Assets/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -13,13 +13,40 @@ public class MainMenuManager : MonoBehaviour {
     private AsyncOperation sync;
     private bool startAnimation;
 
+    public Slider options_VolumeSlider;
+    public Dropdown options_ResolutionDrop;
+    public Dropdown options_FullOrWindDrop;
+    public Dropdown options_ControlSchemeDrop;
+
+    private string volumeKey = "VOLUME_VALUE";
+    private string resolutionKey = "RESOLUTION_VALUE";
+    private string fullscreenKey = "FULLSCREEN_VALUE";
+    private string controlSchemeKey = "CONTROLSCHEME_KEY";
+
     public float timeBetween;
     
 
 	void Start () {
+        options_VolumeSlider.value = PlayerPrefs.GetFloat(volumeKey, 0.75f);
+        options_FullOrWindDrop.value = PlayerPrefs.GetInt(fullscreenKey, 0);
+        options_ResolutionDrop.value = PlayerPrefs.GetInt(resolutionKey, 0);
+        options_ControlSchemeDrop.value = PlayerPrefs.GetInt(controlSchemeKey, 0);
+
         ProgressBar.value = 0;
         Time.timeScale = 1;
         LoadingContainer.SetActive(false);
+        OptionsContainer.SetActive(false);
+        MainContainer.SetActive(true);
+
+        if(options_FullOrWindDrop.value == 0)
+        {
+            Screen.fullScreen = true;
+        }
+        else
+        {
+            Screen.fullScreen = false;
+        }
+
 	}
 
     void Update()
@@ -64,6 +91,26 @@ public class MainMenuManager : MonoBehaviour {
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    public void Options_Save()
+    {
+        PlayerPrefs.SetInt(fullscreenKey, options_FullOrWindDrop.value);
+        PlayerPrefs.SetInt(controlSchemeKey, options_ControlSchemeDrop.value);
+        PlayerPrefs.SetInt(resolutionKey, options_ResolutionDrop.value);
+        PlayerPrefs.SetFloat(volumeKey, options_VolumeSlider.value);
+
+        if (options_FullOrWindDrop.value == 0)
+        {
+            Screen.fullScreen = true;
+        }
+        else
+        {
+            Screen.fullScreen = false;
+        }
+
+        MainContainer.SetActive(true);
+        OptionsContainer.SetActive(false);
     }
 
     IEnumerator LoadingScreen(string whatScene)
