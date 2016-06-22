a88fd8cd990e426070dad1a38242d33d0632225e
Nathan Wentworth <nwentworth95@gmail.com>
2016-06-22 14:42:33 -0400

    added ui to the main tutorial

--- Assets/Scripts/GameModes/Tutorial/TutorialManager.cs
+++ Assets/Scripts/GameModes/Tutorial/TutorialManager.cs
@@ -1,27 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour {
 
-    public MovementObjective objective1;
+  public MovementObjective objective1;
+  public Text tutText;
+  private int tutIndex;
+  private bool objTrigger;
+  private string[] tutArr = {"Use WASD or the left analog stick to move to the next objective", "Use the mouse or right analog stick to look around", "Use the left mouse button to take photos"};
 
 	void Start () {
-        LockMouse.Lock();
-        Time.timeScale = 1;
+    LockMouse.Lock();
+    Time.timeScale = 1;
+    tutIndex = 0;
+    TutorialTextDisp();
 	}
+
+  void TutorialTextDisp() {
+    print(tutArr[tutIndex]);
+    if (tutText == null) {
+      return;
+    } else {
+      tutText.text = tutArr[tutIndex];
+    }
+    if (tutIndex <= 1 && objTrigger == false) {StartCoroutine(Timer(5));}
+  }
 	
-	// Update is called once per frame
 	void Update () {
-        if (objective1 == null)
-        {
-            return;
-        }
-        else {
-            if (objective1.MovementObjectiveTriggered)
-            {
-                Debug.Log("Objective 1 Triggered");
-                objective1.MovementObjectiveTriggered = false;
-            }
-        }
+    if (objective1 == null) {
+      return;
+    } else {
+      if (objective1.MovementObjectiveTriggered) {
+        Debug.Log("Objective 1 Triggered");
+        objTrigger = true;
+        objective1.MovementObjectiveTriggered = false;
+      }
+    }
 	}
+
+  IEnumerator Timer(float time) {
+    yield return new WaitForSeconds(time);
+    tutIndex++;
+    TutorialTextDisp();
+  }
 }
